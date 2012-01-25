a7de7c0ba4cc7e8cfadb285ba30cf56efcb53af0
marco-manti <marco.manti@gmail.com>
2012-01-25 18:57:52 +0300

    Add template files for Vzlom

---
+++ trunk/assets/js/default.js
@@ -0,0 +1,12 @@
+/**
+  * Default template js file
+  * @package M2 Micro Framework
+  * @subpackage Template
+  * @author Alexander Chaika
+  */
+
+$(document).ready(function(){
+    $('#popup .head').click(function() {
+        $('#popup').hide();
+    });
+});
--- trunk/templates/default/index.php
+++ trunk/templates/default/index.php
@@ -5,22 +5,9 @@
       * @subpackage Template
       * @author Alexander Chaika
       */
-      
-    ;
 ?>
 <html>
-    <head>
-        <title><?php echo strip_tags($options['title']); ?></title>
-        <link type="text/css" rel="stylesheet" href="<?php echo $this->config['http_host']; ?>/assets/css/default.css" />
-        <script type="text/javascript" src="<?php echo $this->config['http_host']; ?>/assets/js/jquery.min.js"></script>
-        <script type="text/javascript">
-            $(document).ready(function(){
-                $('#popup .head').click(function() {
-                    $('#popup').hide();
-                });
-            });
-        </script>
-    </head>
+
     <body>
         <div id="popup">
             <div class="head">[X] <?php echo T('Close'); ?></div>
