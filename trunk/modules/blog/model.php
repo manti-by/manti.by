33595eb7c12deb3a2689910137147c14abcf21b7
marco-manti <marco.manti@gmail.com>
2012-01-27 17:00:07 +0300

    Add blog module

---
+++ trunk/modules/blog/model.php
@@ -0,0 +1,30 @@
+<?php
+    defined('M2_MICRO') or die('Direct Access to this location is not allowed.');
+
+    /**
+      * @desc Blog Model class
+      * @name $blogModel
+      * @package M2 Micro Framework
+      * @subpackage Modules
+      * @author Alexander Chaika
+      */
+    class BlogModel extends Model {
+        
+        public function getCategory($id){
+            $this->database->query("SELECT * FROM `category` WHERE `id` = " . $id);
+            if ($this->database->getResult() >= 0) {
+                return $this->database->getObjectsArray();
+            } else {
+                return false;
+            }
+        }
+        
+        public function getCategoryItems($id){
+            $this->database->query("SELECT * FROM `items` WHERE `category_id` = " . $id);
+            if ($this->database->getResult() >= 0) {
+                return $this->database->getObjectsArray();
+            } else {
+                return false;
+            }
+        }
+    }
