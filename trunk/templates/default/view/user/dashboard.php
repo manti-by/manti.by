6c002388495b187c5eb16efa4b622a978bdd4236
marco-manti <marco.manti@gmail.com>
2012-06-14 12:59:28 +0300

    New template view schema

---
+++ trunk/templates/default/view/user/dashboard.php
@@ -0,0 +1,14 @@
+<?php
+    /**
+      * @author Alexander Chaika
+      * @package M2 Micro Framework
+      * @subpackage Template
+      * @author Alexander Chaika
+      */
+?>
+<div id="dashboard">
+    <ul>
+        <li><a href="<?php echo Sef::getSef('index.php?module=blog&action=add');?>"><?php echo T('Add new post'); ?></a></li>
+    </ul>
+</div>
+
