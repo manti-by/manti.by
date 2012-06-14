e4064410b48feb386f27d8552f674d16cf2129da
marco-manti <marco.manti@gmail.com>
2012-06-14 18:59:16 +0300

    New templates structure for blog

---
+++ trunk/templates/default/view/blog/list.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+      * @author Alexander Chaika
+      * @package M2 Micro Framework
+      * @subpackage Template
+      * @author Alexander Chaika
+      */
+?>
+<h1><?php echo $options['title']; ?></h1>
+<div id="category">
+    <?php echo $options['data']; ?>
+</div>
