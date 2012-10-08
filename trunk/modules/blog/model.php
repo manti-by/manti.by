3936c49f284cf5fae725d172219932e08d6bd346
marco-manti <marco.manti@gmail.com>
2012-10-08 17:29:20 +0300

    Add getRelations to blog model

--- trunk/modules/blog/model.php
+++ trunk/modules/blog/model.php
@@ -37,11 +37,21 @@
             if (empty($tags)) {
                 return $this->getPosts();
             }
-            
+
             $tags = implode(',', $tags);
             $this->database->query("CALL GET_POSTS_BY_TAGS('$tags', $limit)");
             if ($this->database->getResult() > 0) {
                 return $this->database->getObjectsArray();
+            } else {
+                return false;
+            }
+        }
+
+        public function getRelations($id){
+            $id = (int)$id;
+            $this->database->query("CALL GET_POST_RELATIONS($id)");
+            if ($this->database->getResult() > 0) {
+                return $this->database->getPairs('id', 'name');
             } else {
                 return false;
             }
