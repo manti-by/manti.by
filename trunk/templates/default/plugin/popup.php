8e3633b244e03e00c78d265bc5967a3486308ce3
marco-manti <marco.manti@gmail.com>
2012-10-10 20:12:19 +0300

    Fix popup behaviour

--- trunk/templates/default/plugin/popup.php
+++ trunk/templates/default/plugin/popup.php
@@ -10,6 +10,21 @@
      * @since 0.1
      */
 ?>
+<script type="text/javascript">
+    $(document).ready(function() {
+        $.fn.popupShow = function(data) {
+            $('#popup .content').html(data);
+            $('#popup').show();
+        }
+
+        $.fn.popupHide = function() {
+            var content = $('#popup .content').html();
+            $('#popup .content').html('');
+            $('#popup').hide();
+            return content;
+        }
+    });
+</script>
 <div id="popup">
     <div class="head hide_this">[X] <?php echo T('Close'); ?></div>
     <div class="content"></div>
