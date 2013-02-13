5c8bb1223e1dd9a1f6822ab0694a3a4b396d57af
marco-manti <marco.manti@gmail.com>
2013-02-13 12:18:01 +0300

    [#43635393] Fix click for next and prev actions

--- trunk/templates/default/gallery/ajax.php
+++ trunk/templates/default/gallery/ajax.php
@@ -27,57 +27,74 @@
                 { id : rel_id }
             );
 
-            // Preload image and show
-            var image_block = $('<img src="' + original + '" class="original" rel="' + rel_id + '" />').load(function() {
-                $.fn.loaderHide();
-                $.fn.popupShow(image_block);
-            });
+            // Create image block
+            var image_block = $('<img src="' + original + '" class="original" rel="' + rel_id + '" />');
 
-            return false;
-        });
-
-        $('.original').live('click', function(e) {
-            e.preventDefault();
-
-            // Show loader and get next image
-            $.fn.loaderShow();
-            $.post(
-                '<?php echo Sef::getSef('index.php?module=gallery&action=next'); ?>',
-                { id : $(this).attr('rel') }, buildImage(response)
-            );
-
-            return false;
-        });
-
-        $('.prev').live('click', function(e) {
-            e.preventDefault();
-
-            // Show loader and get next image
-            $.fn.loaderShow();
-            $.post(
-                '<?php echo Sef::getSef('index.php?module=gallery&action=prev'); ?>',
-                { id : $(this).attr('rel') }, buildImage(response)
-            );
+            // Wait while loading and bind click
+            image_block.load(buildImageBlock(image_block, rel_id)).bind('click', nextImage);
 
             return false;
         });
 
         buildImage = function(response) {
             if (response.result == 'success') {
-                var image_block = $('<img src="' + response.original + '" class="original" rel="' + response.id + '" />').load(function() {
-                    $.fn.loaderHide();
-                    $.fn.popupShow(image_block);
+                // Create image block
+                var image_block = $('<img src="' + response.original + '" class="original" rel="' + response.id + '" />');
 
+                // Wait while loading and bind click and track
+                image_block.load(buildImageBlock(image_block, response.id)).load(function(){
                     // Ping view counter
                     $.post(
                         '<?php echo Sef::getSef('index.php?module=file&action=track'); ?>',
                         { id : response.id }
                     );
-                });
+                }).bind('click', nextImage);
             } else {
-                var popup_block = $(response.error);
-                $.fn.popupShow(popup_block);
+                if (response.error) {
+                    var popup_block = $(response.error);
+                    $.fn.popupShow(popup_block);
+                } else {
+                    $.fn.loaderHide();
+                }
             }
+        }
+
+        buildImageBlock = function(image_block, rel_id) {
+            // Popup blocks
+            var next_pointer = $('<div class="next" rel="' + rel_id + '"></div>').bind('click', nextImage);
+            var prev_pointer = $('<div class="prev" rel="' + rel_id + '"></div>').bind('click', prevImage);
+            var wrapper = $('<div class="image-wrapper"></div>');
+
+            // Build block wrapper and show
+            wrapper.append(prev_pointer).append(image_block).append(next_pointer);
+            $.fn.popupShow(wrapper);
+            $.fn.loaderHide();
+        }
+
+        nextImage = function(event) {
+            event.preventDefault();
+
+            // Show loader and get next image
+            $.fn.loaderShow();
+            $.post(
+                '<?php echo Sef::getSef('index.php?module=gallery&action=next'); ?>',
+                { id : $(this).attr('rel') }, buildImage
+            );
+
+            return false;
+        }
+
+        prevImage = function(event) {
+            event.preventDefault();
+
+            // Show loader and get next image
+            $.fn.loaderShow();
+            $.post(
+                '<?php echo Sef::getSef('index.php?module=gallery&action=prev'); ?>',
+                { id : $(this).attr('rel') }, buildImage
+            );
+
+            return false;
         }
     });
 </script>
