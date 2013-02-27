e9cac99d71328a46048f3d0f99f8dade6ecab558
marco-manti <marco.manti@gmail.com>
2013-02-27 16:10:53 +0300

    [#44528397] Move PHP metric runners to utils

---
+++ utils/pdepend.bat
@@ -0,0 +1,3 @@
+@echo off
+"C:\www\php5\pdepend" --jdepend-chart=..\docs\pdepend\jdepend-chart.svg --jdepend-xml=..\docs\pdepend\jdepend.xml --overview-pyramid=..\docs\pdepend\overview.svg --summary-xml=docs\pdepend\summary.xml ..\trunk
+PAUSE
