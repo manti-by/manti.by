641f4fbdab7484edbf3603a7f7bb756ff956227b
marco-manti <marco.manti@gmail.com>
2013-03-28 19:17:01 +0300

    Minor fixes

--- database/2013-03-28_81_extend_gallery_name.sql
+++ database/2013-03-28_81_extend_gallery_name.sql
@@ -1,17 +1,2 @@
-DROP PROCEDURE IF EXISTS `SEARCH_GALLERY`;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `SEARCH_GALLERY`(IN `_query` text)
-BEGIN
-  SELECT DISTINCT g.`id`, g.`path`, g.`name`, g.`alias`, g.`description`, g.`metadesc`, g.`timestamp`
-  FROM `gallery` g
-  LEFT JOIN `gallery_tags` gt ON gt.`gallery_id` = g.`id`
-  LEFT JOIN `tags` t ON t.`id` = gt.`tag_id`
-  WHERE g.`name` LIKE CONCAT('%', _query, '%')
-     OR g.`alias` LIKE CONCAT('%', _query, '%')
-     OR g.`description` LIKE CONCAT('%', _query, '%')
-     OR g.`metadesc` LIKE CONCAT('%', _query, '%')
-     OR t.`name` LIKE CONCAT('%', _query, '%')
-     OR t.`alias` LIKE CONCAT('%', _query, '%');
-END
-;;
-DELIMITER ;
+ALTER TABLE `gallery`
+    MODIFY COLUMN `name`  varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL AFTER `path`;
