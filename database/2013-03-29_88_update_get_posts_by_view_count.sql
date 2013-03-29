d510c9bb383ceb15d2e86ac91c1b69f94b0662df
marco-manti <marco.manti@gmail.com>
2013-03-29 17:02:35 +0300

    Fix get posts by view count

--- database/2013-03-29_88_update_get_posts_by_view_count.sql
+++ database/2013-03-29_88_update_get_posts_by_view_count.sql
@@ -2,6 +2,8 @@ DROP PROCEDURE IF EXISTS `GET_POSTS_BY_VIEW_COUNT`;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `GET_POSTS_BY_VIEW_COUNT`(IN `_limit` int, IN `_with_covers` tinyint)
 BEGIN
+		CALL ALL_POSTS();
+
 		IF _with_covers > 0 THEN
 		    PREPARE stmt FROM "SELECT p.*
 					FROM `all_posts` AS p
@@ -15,9 +17,7 @@ BEGIN
 					LIMIT ?;";
 		END IF;
 
-    CALL ALL_POSTS();
-
-		SET @rows = _limit;
+    SET @rows = _limit;
     EXECUTE stmt USING @rows;
 END
 ;;
