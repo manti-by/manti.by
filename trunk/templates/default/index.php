0eeb2c3a768928f2f31653dda51e85d086c26cfb
marco-manti <marco.manti@gmail.com>
2013-04-02 16:35:51 +0300

    [#43635923] Add google analitycs

--- trunk/templates/default/index.php
+++ trunk/templates/default/index.php
@@ -64,6 +64,19 @@
         <script type="text/javascript" src="<?php echo Application::$config['http_host']; ?>/templates/default/default.js"></script>
     </head>
     <body>
+        <script type="text/javascript">
+
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-39793152-1']);
+            _gaq.push(['_trackPageview']);
+
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+
+        </script>
         <?php echo $this->getContents('plugin', 'gototop'); ?>
         <?php echo $this->getContents('plugin', 'ajax'); ?>
         <?php echo $this->getContents('plugin', 'popup'); ?>
