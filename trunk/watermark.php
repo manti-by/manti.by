047171f4adb18499912a1a3ce4d9be0f1d34dbc8
marco-manti <marco.manti@gmail.com>
2013-05-29 13:15:31 +0300

    [#50253649] Fix expires date format, update htaccess

--- trunk/watermark.php
+++ trunk/watermark.php
@@ -58,7 +58,7 @@
     if ($image = Model::getModel('gallery')->addWatermark($_SERVER['REQUEST_URI'])) {
         header('HTTP/1.1 200 OK');
 
-        header('Expires: ' . gmdate(DATE_RFC822, strtotime("1 year")));
+        header('Expires: ' . gmdate('D, d M Y H:i:s', strtotime("1 year")) . ' GMT');
         header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
         header('Cache-Control: public, must-revalidate, proxy-revalidate');
         header('Pragma: public');
