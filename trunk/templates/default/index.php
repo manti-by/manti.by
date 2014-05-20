7e32198240ac3306c8a8363eab1a2b8ea9808143
Alexander Chaika <manti.by@gmail.com>
2014-05-20 13:59:46 +0300

    Update changes from prod

--- trunk/templates/default/index.php
+++ trunk/templates/default/index.php
@@ -74,20 +74,15 @@
     </head>
     <body>
         <script type="text/javascript">
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-            var _gaq = _gaq || [];
-            _gaq.push(['_setAccount', 'UA-39793152-1']);
-            _gaq.push(['_trackPageview']);
-
-            (function() {
-                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-            })();
-
+            ga('create', 'UA-39793152-1', 'manti.by');
+            ga('send', 'pageview');
         </script>
 
-        <!-- Yandex.Metrika counter -->
         <script type="text/javascript">
             (function (d, w, c) {
                 (w[c] = w[c] || []).push(function() {
@@ -112,7 +107,6 @@
             })(document, window, "yandex_metrika_callbacks");
         </script>
         <noscript><div><img src="//mc.yandex.ru/watch/20835088" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-        <!-- /Yandex.Metrika counter -->
 
         <?php echo $this->getContents('plugin', 'gototop'); ?>
         <?php echo $this->getContents('plugin', 'ajax'); ?>
