4d599d20c195e6854fa2e66725bca64b14a1e305
manti-by <manti.by@gmail.com>
2017-05-12 13:24:36 +0300

    Add tag search

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -4,6 +4,6 @@ from blog import views
 
 
 urlpatterns = [
-    url(r'^$', views.index, name='post_list'),
+    url(r'^$', views.index, name='blog'),
     url(r'^(?P<slug>[-\w]+)/$', views.BlogView.as_view(template_name='blog/post.html'), name='post'),
 ]
--- app/blog/views.py
+++ app/blog/views.py
@@ -5,8 +5,12 @@ from blog.models import Post
 
 
 def index(request):
-    item_list = Post.objects.order_by('-created')[:6]
-    return render(request, 'blog/list.html', {'item_list': item_list})
+    tag = request.GET.get('tag')
+    if tag:
+        item_list = Post.objects.filter(tags__slug=tag).order_by('-created')[:6]
+    else:
+        item_list = Post.objects.order_by('-created')[:6]
+    return render(request, 'blog/list.html', {'item_list': item_list, 'tag': tag})
 
 
 class BlogView(DetailView):
