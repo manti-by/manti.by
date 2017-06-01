486dac84cebbc2250bd5296357dbdf093b0c7145
manti-by <manti.by@gmail.com>
2017-06-01 18:25:53 +0300

    Add sidebar

--- app/core/context_processors/common.py
+++ app/core/context_processors/common.py
@@ -1,6 +1,10 @@
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
+from blog.models import Post
+from gallery.models import Gallery
+from taggit.models import Tag
+
 
 def global_template_vars(request):
     return {
@@ -10,5 +14,7 @@ def global_template_vars(request):
         'ABSOLUTE_URL': request.build_absolute_uri(),
         'META_TITLE': _(settings.META_TITLE),
         'META_DESCRIPTION': _(settings.META_DESCRIPTION),
-        'BASE_URL': settings.BASE_URL
+        'BASE_URL': settings.BASE_URL,
+        'tags': Tag.objects.all()[:10],
+        'posts': Post.objects.all()[:10]
     }
