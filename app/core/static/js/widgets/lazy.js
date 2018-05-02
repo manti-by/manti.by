dce92a74da74192cbc66b80b831c76d60de7cda3
manti-by <manti.by@gmail.com>
2018-05-02 16:52:53 +0300

    Add lazy images observer

--- app/core/static/js/widgets/lazy.js
+++ app/core/static/js/widgets/lazy.js
@@ -3,11 +3,18 @@
     'use strict';
 
     $.fn.initLazyImages = function() {
+        let observer = new IntersectionObserver(changes => {
+            for (const change of changes) {
+                $(change.target).attr('src', $(change.target).attr('data-src'));
+                $(change.target).on('load', function() {
+                    $(change.target).removeAttr('data-src');
+                });
+
+            }
+        });
+
         $.each($('img.lazy'), function() {
-          $(this).attr('src', $(this).attr('data-src'));
-          $(this).on('load', function() {
-            $(this).removeAttr('data-src');
-          });
+            observer.observe(this);
         });
     }
 
