76809680525716a7d88cabfa778489dcd2e9fcd9
manti-by <manti.by@gmail.com>
2018-06-12 12:34:54 +0300

    Fix nd update gallery library

--- app/core/static/js/libs/gallery.js
+++ app/core/static/js/libs/gallery.js
@@ -2,17 +2,17 @@
 
     'use strict';
 
-    $.gallery =  {
+    class Gallery {
 
-        image: null,
-        overlay: null,
+        constructor() {
+            this.image = null;
+            this.overlay = null;
+            this.id_map = [];
+            this.current_id = null;
+        }
 
-        id_map: [],
-        current_id: null,
-
-        init: function() {
-            var view = this,
-                close_pointer = $('<div class="close"><i class="material-icons">close</i></div>'),
+        init() {
+            let close_pointer = $('<div class="close"><i class="material-icons">close</i></div>'),
                 next_pointer = $('<div class="next"><i class="material-icons">chevron_right</i></div>'),
                 prev_pointer = $('<div class="prev"><i class="material-icons">chevron_left</i></div>'),
                 overlay = $('<div id="overlay" class="hidden"></div>');
@@ -23,71 +23,69 @@
             $('body').append(overlay);
 
             // Bind overlay actions
-            overlay.bind('click', function() { view.next(); });
-            next_pointer.bind('click', function() { view.next(); });
-            prev_pointer.bind('click', function() { view.prev(); });
+            overlay.bind('click', () => { this.next(); });
+            next_pointer.bind('click', () => { this.next(); });
+            prev_pointer.bind('click', () => { this.prev(); });
 
-            close_pointer.bind('click', function(event) {
-                event.stopPropagation();
-                view.hide();
+            close_pointer.bind('click', (e) => {
+                e.stopPropagation();
+                this.hide();
             });
 
-            $(window).bind('resize', function() { view.resize(); });
-            $(document).bind('keydown', function(event) {
-                if (event.keyCode === 27) {
-                    view.hide();
+            $(window).bind('resize', () => { this.resize(); });
+            $(document).bind('keydown', (e) => {
+                if (e.keyCode === 27) {
+                    this.hide();
                 }
-                if (event.keyCode === 39) {
-                    view.next();
+                if (e.keyCode === 39) {
+                    this.next();
                 }
-                if (event.keyCode === 37) {
-                    view.prev();
+                if (e.keyCode === 37) {
+                    this.prev();
                 }
             });
 
             // Bind image actions
-            $.each($('.preview'), function() {
-                var id = $(this).data('id'),
-                    link = $(this).attr('href');
+            $.each($('.preview'), (i, el) => {
+                let id = $(el).data('id'),
+                    link = $(el).attr('href');
 
-                $(this).bind('click', function(event) {
-                    event.preventDefault();
-                    view.build(id, link);
+                $(el).bind('click', (e) => {
+                    e.preventDefault();
+                    this.build(id, link);
                 });
 
-                view.id_map.push([id, link]);
+                this.id_map.push([id, link]);
             });
 
-            view.overlay = overlay;
-        },
+            this.overlay = overlay;
+        };
 
-        build: function(id, link) {
-            var view = this;
+        build(id, link) {
+            $.loaderShow();
 
-            $.fn.loaderShow();
+            this.overlay.find('.original').remove();
+            this.image = $('<img src="' + link + '" class="original resizible shadow" />');
+            this.image.on('load', () => {
+                this.current_id = id;
+                this.overlay.append(this.image);
 
-            view.overlay.find('.original').remove();
-            view.image = $('<img src="' + link + '" class="original resizible shadow" />');
-            view.image.on('load', function() {
-                view.current_id = id;
-                view.overlay.append(view.image);
-
-                view.open();
-                view.resize();
-                $.fn.loaderHide();
+                this.open();
+                this.resize();
+                $.loaderHide();
             });
-        },
+        };
 
-        resize: function() {
-            var shift_x = ($(window).width() - this.image.width()) / 2,
+        resize() {
+            let shift_x = ($(window).width() - this.image.width()) / 2,
                 shift_y = ($(window).height() - this.image.height()) / 2;
 
             if (shift_x > 0) this.image.css('left', shift_x);
             if (shift_y > 0) this.image.css('top', shift_y);
-        },
+        };
 
-        next: function() {
-            for (var i = 0; i < this.id_map.length; i++) {
+        next() {
+            for (let i = 0; i < this.id_map.length; i++) {
                 if (this.current_id === this.id_map[i][0]) {
                     if (i + 1 === this.id_map.length) {
                         this.build(this.id_map[0][0], this.id_map[0][1]);
@@ -97,10 +95,10 @@
                     return;
                 }
             }
-        },
+        };
 
-        prev: function() {
-            for (var i = 0; i < this.id_map.length; i++) {
+        prev() {
+            for (let i = 0; i < this.id_map.length; i++) {
                 if (this.current_id === this.id_map[i][0]) {
                     if (i - 1 < 0) {
                         this.build(this.id_map[this.id_map.length][0], this.id_map[this.id_map.length][1]);
@@ -110,14 +108,17 @@
                     return;
                 }
             }
-        },
+        };
 
-        open: function() {
+        open() {
             this.overlay.removeClass('hidden');
-        },
+        };
 
-        hide: function() {
+        hide() {
             this.overlay.addClass('hidden');
-        }
-    };
+        };
+    }
+
+    $.gallery = new Gallery();
+
 })(jQuery);
