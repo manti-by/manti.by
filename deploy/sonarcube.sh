dcaf6e23b099b41a077cf9637970bae08c3c1b43
manti-by <manti.by@gmail.com>
2018-06-13 14:07:23 +0300

    Add SonarCube script

---
+++ deploy/sonarcube.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+sonar-scanner \
+  -Dsonar.projectKey=manti.by \
+  -Dsonar.organization=manti-by-github \
+  -Dsonar.sources=../app \
+  -Dsonar.host.url=https://sonarcloud.io \
+  -Dsonar.login=17d12100b8cf288e141f6bb443a5a35f1f1de83e
