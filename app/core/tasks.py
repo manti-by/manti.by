73aa71c0e4f067720d29d91321f47df02118ce11
manti-by <manti.by@gmail.com>
2018-06-14 10:09:18 +0300

    Add check_posts_state command, remove celery tasks

--- app/core/tasks.py
+++ app/core/tasks.py
@@ -1,30 +1,7 @@
-import os
 import subprocess
 
 from core.celery import app
 from blog.constants import MP3_PREVIEW, OGG_PREVIEW, OGG_RELEASE
-
-
-@app.on_after_configure.connect
-def setup_periodic_tasks(sender, **kwargs):
-    sender.add_periodic_task(600.0, check_posts_state.s(),
-                             name='Check Posts state every 10 minutes')
-
-
-@app.task
-def check_posts_state():
-    from blog.models import Post
-    for post in Post.objects.all():
-        post.ogg_release_ready = os.path.exists(post.release_ogg_file) \
-            and os.path.getsize(post.release_ogg_file) > 0
-
-        post.mp3_preview_ready = os.path.exists(post.preview_mp3_file) \
-            and os.path.getsize(post.preview_mp3_file) > 0
-
-        post.ogg_preview_ready = os.path.exists(post.preview_ogg_file) \
-            and os.path.getsize(post.preview_ogg_file) > 0
-
-        post.save()
 
 
 @app.task
