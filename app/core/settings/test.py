8ed4c2260a587e9f6e95d1dafd60a179edbbc01e
manti-by <manti.by@gmail.com>
2018-06-22 17:07:26 +0300

    Update CI config

--- app/core/settings/test.py
+++ app/core/settings/test.py
@@ -16,7 +16,7 @@ MEDIA_ROOT = '/tmp/'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join('tmp', 'db.sqlite3'),
     }
 }
 
