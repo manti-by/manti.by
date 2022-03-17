33a26f6f33fe861d5ff2e46e2b41fcc18506c2e5
Alexander Chaika <manti.by@gmail.com>
2022-03-17 23:18:36 +0300

    Add rocket app

---
+++ app/src/main.rs
@@ -0,0 +1,91 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+use rocket::response::content;
+use rocket::response::Redirect;
+use rocket_contrib::serve::StaticFiles;
+use rocket_contrib::templates::Template;
+use serde_json::json;
+use std::fs::File;
+use std::io::BufReader;
+
+#[get("/")]
+fn index() -> Template {
+    let context = json!({
+        "featured_image": {
+            "id": 44,
+            "display": "/content/gallery/44_Quattro-Corti.display.webp",
+            "preview": "/content/gallery/44_Quattro-Corti.preview.webp",
+            "thumbnail": "/content/gallery/44_Quattro-Corti.thumbnail.webp",
+        },
+        "featured_posts": [],
+        "latest_images": [],
+        "latest_posts": [],
+        "tags": [],
+        "posts": [],
+    });
+    Template::render("index", context)
+}
+
+#[get("/about")]
+fn about() -> Template {
+    let context = json!({});
+    Template::render("about", context)
+}
+
+#[get("/resume")]
+fn resume() -> Redirect {
+    Redirect::permanent(uri!(about))
+}
+
+#[get("/copyrights")]
+fn copyrights() -> Redirect {
+    Redirect::permanent(uri!(about))
+}
+
+#[get("/blog")]
+fn blog() -> Template {
+    let context = json!({});
+    Template::render("partial", context)
+}
+
+fn get_gallery_context() -> serde_json::Result<serde_json::Value> {
+    let file = File::open("/home/manti/www/manti.io/content/gallery.json").unwrap();
+    let reader = BufReader::new(file);
+    serde_json::from_reader(reader)
+}
+
+#[get("/gallery")]
+fn gallery() -> Template {
+    match get_gallery_context() {
+        Ok(context) => {
+            Template::render("gallery", context)
+        },
+        Err(e) => {
+            let context = json!({"message": e.to_string()});
+            Template::render("error", context)
+        }
+    }
+}
+
+#[get("/dashboard")]
+fn dashboard() -> Template {
+    let context = json!({});
+    Template::render("dashboard", context)
+}
+
+#[get("/api")]
+fn api() -> content::Json<&'static str> {
+    content::Json("{[]}")
+}
+
+fn main() {
+    rocket::ignite()
+        .attach(Template::fairing())
+        .mount("/static", StaticFiles::from("../static"))
+        .mount("/content", StaticFiles::from("../content"))
+        .mount("/", routes![index, blog, gallery, api, about, dashboard])
+        .launch();
+}
