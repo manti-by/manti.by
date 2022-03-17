8e51b2f3eb191aa9c346dbe2bb837c7639da13ea
Alexander Chaika <manti.by@gmail.com>
2022-03-17 23:19:50 +0300

    Add static files and templates

---
+++ app/static/js/widgets/ajax.js
@@ -0,0 +1,37 @@
+(($) => {
+  'use strict'
+
+  $.initBlogAjax = (tag) => {
+    const footer = $('footer')
+    const options = { type: 'html', limit: 6 }
+    let start = 6; let locked = false
+
+    if (tag) options.tag = tag
+    $(window).on('scroll', () => {
+      if (footer.offset().top < $(window).scrollTop() + $(window).height()) {
+        if (!locked) {
+          locked = true
+          options.start = start
+          $.loaderShow()
+
+          $.get('/api/', options, (response) => {
+            if (response.status === 200) {
+              start += 6
+              for (const post of response.data) {
+                $('.partial .posts').append(post)
+              }
+
+              $.initLazyImages()
+              $.player.updateActivePosts()
+              if (response.data.length) locked = false
+            } else {
+              DEBUG && console.error(response.message)
+            }
+
+            $.loaderHide()
+          })
+        }
+      }
+    })
+  }
+})(jQuery)
