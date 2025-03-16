736cc4b6a75b882579c846c83bb5627f48e7ea8c
Alexander Chaika <manti.by@gmail.com>
2025-03-16 21:31:31 +0300

    Fix fronpage, update packages

--- manti_by/apps/core/views.py
+++ manti_by/apps/core/views.py
@@ -1,17 +1,18 @@
 import logging
 
 from django.core.cache import cache
-from django.http import Http404
+from django.http import Http404, HttpRequest
 from django.shortcuts import render
 
 from manti_by.apps.blog.models import Post
 from manti_by.apps.core.utils import update_cache
 from manti_by.apps.gallery.models import Image
 
+
 logger = logging.getLogger(__name__)
 
 
-def index(request):
+def index(request: HttpRequest) -> str:
     cache_key = f"index-{request.LANGUAGE_CODE}"
     cached_data = cache.get(cache_key)
     if cached_data:
@@ -24,7 +25,7 @@ def index(request):
         else:
             latest_images = Image.objects.all().order_by("-order")[:6]
 
-        featured_posts = Post.objects.filter(tags__name__iexact="featured")[:3]
+        featured_posts = Post.objects.filter(tags__name__iexact="featured").order_by("created")[:3]
         latest_posts = (
             Post.objects.exclude(pk__in=[p.pk for p in featured_posts])
             .filter(tags__name__iexact="front")
@@ -44,10 +45,10 @@ def index(request):
         return update_cache(cache_key, cached_data)
     except Exception as e:
         logger.exception(e)
-        raise Http404
+        raise Http404 from e
 
 
-def static(request, page):
+def static(request: HttpRequest, page: str) -> str:
     cache_key = f"static-{page}-{request.LANGUAGE_CODE}"
     cached_data = cache.get(cache_key)
     if cached_data:
@@ -57,4 +58,4 @@ def static(request, page):
         return update_cache(cache_key, cached_data)
     except Exception as e:
         logger.exception(e)
-        raise Http404
+        raise Http404 from e
